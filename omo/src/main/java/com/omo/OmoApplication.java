569d69a48986aae01d740fa3ca2ace2b85e99fa5
Haan0819 <hs330819@naver.com>
2023-02-20 15:27:36 +0900

    New Project

--- omo/src/main/java/com/omo/OmoApplication.java
+++ omo/src/main/java/com/omo/OmoApplication.java
@@ -3,7 +3,7 @@ package com.omo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class OmoApplication {
 
 	public static void main(String[] args) {
