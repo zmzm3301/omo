4bdc354fc9c7fac234be3692fc829725aa286430
unknown <zmzm3301>
2023-02-21 15:03:15 +0900

    깃 연결

--- omo/src/main/omo/src/Layout.js
+++ omo/src/main/omo/src/Layout.js
@@ -5,28 +5,23 @@ import signin_img from './static/images/login.png';
 import './App.css';
 import { useEffect, useState } from "react";
 import axios from 'axios'
-
+import $ from 'jquery'
 
 const Layout = () => {
-    const [person, setPerson] = useState("")
-    const [names, setname] = useState("")
-    const [jobs, setjob] = useState("")
+    const [person, setPerson] = useState('')
 
-    const nameoption = (e) => {
-        setname(e.target.value)
-    }
-    const joboption = (e) => {
-        setjob(e.target.value)
-    }
+    const names = document.getElementById('name')
+    const jobs = document.getElementById('job')
 
-    const form = new FormData();
-    form.append('park', 'veneas');
-
-    const onclickLogin = () => {
+    const onclickLogin = (event) => {
+        event.preventDefault();
         axios({
             url: '/persons',
             method: 'post',
-            data: form
+            data: {
+                name: names.value,
+                job: jobs.value
+            }
         })
             .then(function a(response) {
                 console.log(response)
@@ -37,15 +32,23 @@ const Layout = () => {
     }
 
     useEffect(() => {
-        axios.get('/test').then(response => setPerson(response.data))
+        axios.get('/persons').then(response => setPerson(response.data))
             .catch(error => console.log(error))
+                
     }, []);
 
+    const info = document.getElementById('info')
+    
+    for(var i = 0; i < person.length; i++) {
+        console.log(person[i].name)
+        const newDiv = document.createElement('div')
+        newDiv.style.border="1px solid black";
+        newDiv.style.width="180px";
+        newDiv.append(person[i].name + "  " + person[i].job)
+        info.append(newDiv)
+    }
     console.log(person)
-    const name = person.name
-    const job = person.job
-    const id = person.id
-
+   
 
     return (
         <div>
@@ -59,14 +62,19 @@ const Layout = () => {
                 </header>
                 <main className="main_cont">
                     <Outlet />
-                    <input onChange={nameoption} value={names} />
-                    <input onChange={joboption} value={jobs} />
-                    <button onClick={onclickLogin}>submit</button>
-                    {id} {name} {job}
+                    <form onSubmit={onclickLogin}>
+                        <input id="name" name="name" />
+                        <input id="job" name="job" />
+                        <button type="submit" value="입력" >입력</button>
+                    </form>
+                    
+                    <div id="info">
+                    </div>
                 </main>
                 <Footer />
             </div>
         </div>
+        
     )
 }
 
--- omo/src/main/omo/src/Main.js
+++ omo/src/main/omo/src/Main.js
@@ -4,6 +4,9 @@ function Main() {
     return (
         <>
             <section>메인 컨텐츠
+                <div>
+                    <h3>singup</h3>
+                </div>123444
             </section>
         </>
     )
